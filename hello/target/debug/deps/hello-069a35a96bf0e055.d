c9bb1a647535a506c715e5811671b190152a53dd
Posandu Mapa <posandumapa@gmail.com>
2022-04-08 18:48:50 +0530

    hello over

---
+++ hello/target/debug/deps/hello-069a35a96bf0e055.d
@@ -0,0 +1,5 @@
+C:\development\lab\learning-rust\hello\target\debug\deps\hello-069a35a96bf0e055.rmeta: src\main.rs
+
+C:\development\lab\learning-rust\hello\target\debug\deps\hello-069a35a96bf0e055.d: src\main.rs
+
+src\main.rs:
